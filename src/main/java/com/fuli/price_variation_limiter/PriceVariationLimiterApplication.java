b458d3c13138caa1064474db64bd60218fb77759
Fu, Robin <44071928@qq.com>
2025-02-11 12:07:33 +0800

    project initialize

---
+++ src/main/java/com/fuli/price_variation_limiter/PriceVariationLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.fuli.price_variation_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceVariationLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceVariationLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuli/price_variation_limiter/PriceVariationLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuli.price_variation_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceVariationLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
